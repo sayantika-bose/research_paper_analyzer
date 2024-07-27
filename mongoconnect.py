035dfc8325af872b3d2874103eeb6269fb84a533
Sai Sudhane <saidon7321@gmail.com>
2024-07-27 21:25:36 +0530

    Version till 27-07-2024

--- mongoconnect.py
+++
@@ -1,8 +0,0 @@
-from pymongo import MongoClient
-
-client = MongoClient(
-    "mongodb+srv://sudhaneg8321:<password>@gemini.nx6gfiz.mongodb.net/"
-)
-dbName = "gemini_project"
-collectionName = "Research_Paper"
-collection = client[dbName][collectionName]
